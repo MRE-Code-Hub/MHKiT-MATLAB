0dea66918a6015a5417947b92fa01b5458b54c69
Pauly <rpauly@rpauly-33786s.nrel.gov>
2019-09-09 15:43:30 -0600

    multiple spectra on plots

--- wave/graphics/plot_spectrum.m
+++ wave/graphics/plot_spectrum.m
@@ -17,11 +17,13 @@ function figure=plot_spectrum(wave_spectra)
 %         Plot of wave amplitude spectra versus omega
 %     
 %     """
-
-Hm0=significant_wave_height(wave_spectra);
-
 figure=plot(wave_spectra.frequency*2*3.14,wave_spectra.spectrum/(2*3.14));
-format_Spec='Spectrum: %s, Tp= %f, Hm0= %f';
-title(sprintf(format_Spec,wave_spectra.type,wave_spectra.Tp,Hm0));
 xlabel('Omega (^{rad}/_{s})')
 ylabel('Spectral Density (m^{2}s/_{rad})') 
+si=size(wave_spectra.spectrum);
+if si(1)==1
+    Hm0=significant_wave_height(wave_spectra);
+    format_Spec='Spectrum: %s, Tp= %f, Hm0= %f';
+    title(sprintf(format_Spec,wave_spectra.type,wave_spectra.Tp,Hm0));
+end
+    
