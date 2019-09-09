1fa41c53e64a834202ab9c84e3f27bae4b88e933
Pauly <rpauly@rpauly-33786s.nrel.gov>
2019-09-09 14:45:38 -0600

    edited plots

--- wave/graphics/plot_spectrum.m
+++ wave/graphics/plot_spectrum.m
@@ -18,3 +18,10 @@ function figure=plot_spectrum(wave_spectra)
 %     
 %     """
 
+Hm0=significant_wave_height(wave_spectra);
+
+figure=plot(wave_spectra.frequency*2*3.14,wave_spectra.spectrum/(2*3.14));
+format_Spec='Spectrum: %s, Tp= %f, Hm0= %f';
+title(sprintf(format_Spec,wave_spectra.type,wave_spectra.Tp,Hm0));
+xlabel('Omega (^{rad}/_{s})')
+ylabel('Spectral Density (m^{2}s/_{rad})') 
