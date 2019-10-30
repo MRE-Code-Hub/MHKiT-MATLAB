d7e5fd94ec9c1b87e24d72eddf4995c53b4ac343
Pauly <rpauly@nrel.gov>
2019-10-30 13:53:01 -0600

    fist commit discharge

--- mhkit/river/resource/discharge_to_velocity.m
+++ mhkit/river/resource/discharge_to_velocity.m
@@ -1,55 +1,75 @@
-function D=discharge_to_velocity(D,V,varargin)
+function V=discharge_to_velocity(D,polynomial_coefficients)
 
-% Calculates the velocity v given resource 
-%     discharge levels (DataFrame D) and velocity data at an individual
-%     turbine location for varying river discharge levels (DataFrame V).
-%     Typically velocity DataFrame contains 15 points of velocity to 
-%     discharge relations. Additionally this function will return the 
-%     polynomial fit to velocity DataFrame.
+% Calculates velocity given discharge data and the relationship between 
+%     discharge and velocity at an individual turbine
 %     
 %     Parameters
 %     ------------
-%     D : DataFrame
-%        Pandas DataFrame with 10 years of daily discharge [m3/s] values 
-%        specified by either the key 'discharge' or D_keys.
-%     V : DataFrame 
-%        Pandas DataFrame relating discharge [m3/s] and velocity [m/s] at
-%        turbine location      
-%     D_keys : list
-%         If DataFrame D uses key that is not 'discharge' can specify what
-%         key to use for discharge. List must be of length 1.
-%     V_keys : ordered list
-%         If DataFrame V uses key that is not 'discharge' and 'velocity' 
-%         the user can specify what key to use in this ordered list 
-%         where the first position is the discharge key and the second 
-%         value is the velocity key (e.g. ['dis','vel']). List must be 
-%         of length 2. 
-%     order : integer
-%         Order of the polynomial fit
-%     plot_polynomial_fit : bool
-%         returns plot of polynomial fit to data in V
-%     append : bool
-%         Returns DataFrame with only 'v'. If True returns DataFrame
-%         D with 'v' appended in the DataFrame.
-% 
+%     D : structure
+%         Discharge data [m3/s] indexed by time [datetime or s]
+%     polynomial_coefficients : numpy polynomial
+%         List of polynomial coefficients that discribe the relationship between 
+%         discharge and velocity at an individual turbine
+%     
 %     Returns   
 %     ------------
-%     v: array-like 
-%        Uses polynomial fit from data in V to calculate velocity for
-%        each discharge in D. If append is True then 'v' is appended 
-%        to DataFrame D.
-% 
-%     Examples
-%     --------
-%     >>> # Use USGS module to import 10 year daily discharge data
-%     >>> data = river.io.usgs_data("15515500")
-%     >>> df = river.io.usgs_data_to_df(data, data_key='discharge')
-%     >>> # Convert to m3/s
-%     >>> df.discharge = df.discharge / (3.28084)**3
-%     >>> # Calculate the exceedance probability f. and append to df
-%     >>> df = river.resource.exceedance_probability(df, append=True)
-%     >>> # Velocity data for VDC curve (1 turbine location), [discharge, velocity]
-%     >>> vel_data = np.array( [ [515, 1.05], [575, 1.1], [645, 1.25],[850, 1.5], [1240, 1.8], [2917, 2.9]  ] )
-%     >>> V = pd.DataFrame.from_records(vel_data, columns=['discharge', 'velocity'])
-%     >>> # Use both dfs to append VDC data to FDC df 
-%     >>> df = river.resource.discharge_to_velocity(df, V, append=True)
+%     V: pandas DataFrame   
+%         Velocity [m/s] indexed by time [datetime or s]
+
+[own_path,~,~] = fileparts(mfilename('fullpath'));
+modpath= fullfile(own_path, '...');
+P = py.sys.path;
+if count(P,'modpath') == 0
+    insert(P,int32(0),'modpath');
+end
+
+py.importlib.import_module('mhkit');
+
+if (isa(D,'py.pandas.core.frame.DataFrame')~=1)
+    x=size(D.Discharge);
+    li=py.list();
+    if x(2)>1 
+        for i = 1:x(2)
+            app=py.list(D.Discharge(:,i));
+            li=py.pandas_dataframe.lis(li,app);
+            
+        end
+    elseif x(2) ==1 
+        li=D.Discharge;
+    end
+    
+    if any(isdatetime(D.time{1}))
+        si=size(D.time);
+        for i=1:si(2)
+        D.time{i}=posixtime(D.time{i});
+        end
+    end
+    D=py.pandas_dataframe.timeseries_to_pandas(li,D.time,int32(x(2)));
+end
+
+polynomial_coefficients=py.numpy.poly1d(polynomial_coefficients);
+
+Vdf=py.mhkit.river.resource.discharge_to_velocity(D,polynomial_coefficients);
+disp(Vdf)
+
+
+xx=cell(Vdf.axes);
+v=xx{2};
+vv=cell(py.list(py.numpy.nditer(v.values,pyargs("flags",{"refs_ok"}))));
+
+vals=double(py.array.array('d',py.numpy.nditer(Vdf.values)));
+sha=cell(Vdf.values.shape);
+x=int64(sha{1,1});
+y=int64(sha{1,2});
+
+vals=reshape(vals,[x,y]);
+
+si=size(vals);
+ for i=1:si(2)
+    test=string(py.str(vv{i}));
+    newname=split(test,",");
+    
+    V.(newname(1))=vals(:,i);
+    
+ end
+ V.time=double(py.array.array('d',py.numpy.nditer(Vdf.index)));
