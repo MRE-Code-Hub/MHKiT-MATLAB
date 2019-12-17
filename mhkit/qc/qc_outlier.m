f3494e77d435a848c4dec1a0e09cd14740fbf92f
Tim Shippert <tim.shippert@pnnl.gov>
2019-12-17 09:44:10 -0800

    Fixed qc_outlier.m to actually call pecos check_outlier

--- mhkit/qc/qc_outlier.m
+++ mhkit/qc/qc_outlier.m
@@ -1,5 +1,5 @@
 % Wrap QC functions around pecos monitoring check functions
 % This all assumes a pandas dataframe with a datetime index
-function results = qc_corrupt(data,vals)
-  results = struct(py.pecos.monitoring.check_corrupt(data,vals))
+function results = qc_outlier(data,bound)
+  results = struct(py.pecos.monitoring.check_outlier(data,bound))
 end 
